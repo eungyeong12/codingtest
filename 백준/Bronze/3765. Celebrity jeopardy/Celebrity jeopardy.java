9e042e608db0d32ff1452325c2e5210cf3924127
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2024-02-06 14:34:21 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 196 ms, Memory: 17404 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            if(sc.hasNext())
+                System.out.println(sc.nextLine());
+            else
+                break;
+        }
+    }
+}
