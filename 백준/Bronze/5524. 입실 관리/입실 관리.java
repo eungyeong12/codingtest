0348bb27cb45c3372652f9a77cf64080a78aed99
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2024-02-08 11:37:48 +0900

    [Bronze IV] Title: 입실 관리, Time: 216 ms, Memory: 17664 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++) {
+            String s = sc.next();
+            s = s.toLowerCase();
+            System.out.println(s);
+        }
+        sc.close();
+    }
+}
