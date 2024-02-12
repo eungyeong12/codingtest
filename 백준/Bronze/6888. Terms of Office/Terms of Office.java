6b9200df0289998d1c07762e488a6d014d23b225
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2024-02-12 16:45:27 +0900

    [Bronze IV] Title: Terms of Office, Time: 220 ms, Memory: 18268 KB -BaekjoonHub

---
+++ 백준/Bronze/6888. Terms of Office/Terms of Office.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        int[] arr = {2,3,4,5};
+        int lcm = getLCM(arr);
+        while(n1 <= n2) {
+            System.out.println("All positions change in year " + n1);
+            n1 += lcm;
+        }
+    }
+
+    public static int getLCM(int[] arr) {
+        if (arr.length == 1) {
+            return arr[0];
+        }
+
+        int gcd = getGCD(arr[0], arr[1]);
+        int lcm = (arr[0] * arr[1]) / gcd;
+
+        for (int i = 2; i < arr.length; i++) {
+            gcd = getGCD(lcm, arr[i]);
+            lcm = (lcm * arr[i]) / gcd;
+        }
+
+        return lcm;
+    }
+
+    public static int getGCD(int num1, int num2) {
+        if (num1 % num2 == 0) {
+            return num2;
+        }
+        return getGCD(num2, num1 % num2);
+    }
+}
