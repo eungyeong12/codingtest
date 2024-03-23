d96c44f67e80252e3a1bd033a3a5d857a759cb4c
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2024-03-23 19:28:35 +0900

    [Silver II] Title: 좌표 압축, Time: 2436 ms, Memory: 353196 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,3 +1,4 @@
+/* 22212067 조은경 */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,25 +15,24 @@ public class Main {
         s = br.readLine();
         StringTokenizer st = new StringTokenizer(s, " ");
 
-        while(st.hasMoreTokens()) {
+        while(st.hasMoreTokens()) { // list: 원본 데이터, set: 중복을 제거하고 오름차순으로 정렬된 데이터
             int x = Integer.valueOf(st.nextToken());
-            list.add(x);
-            set.add(x);
+            list.add(x); 
+            set.add(x); 
         }
 
         int count = 0;
         Map<Integer, Integer> map = new HashMap<>();
         Iterator iterator = set.iterator();
-        while(iterator.hasNext()) {
-            map.put((Integer) iterator.next(), count++);
+        while(iterator.hasNext()) { // 좌표 X와 X보다 작은 수의 개수를 map에 저장 
+            map.put((Integer) iterator.next(), count++); 
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<n-1; i++) {
+        for(int i=0; i<n; i++) {  // list에 저장된 데이터를 키 값으로 하여 map의 value를 찾아 StringBuilder에 추가
             int key = list.remove(0);
             sb.append(map.get(key)).append(" ");
         }
-        sb.append(map.get(list.get(0)));
 
         System.out.println(sb);
         br.close();
