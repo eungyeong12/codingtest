a8702aa3614a41d154c99eb0961d771d0adce640
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2024-05-04 17:37:52 +0900

    [Silver V] Title: 폴리오미노, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -7,11 +7,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        s = s.replace("XXXX", "AAAA");
-        s = s.replace("XX", "BB");
-        if(s.contains("X"))
-            System.out.println(-1);
-        else
-            System.out.println(s);
+
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(s, ".");
+        StringTokenizer st2 = new StringTokenizer(s, "X");
+        int index = 0;
+
+        while(index <s.length()) {
+            if(s.charAt(index) == '.') {
+                String s2 = st2.nextToken();
+                sb.append(s2);
+                index += s2.length();
+            } else {
+                String s2 = st.nextToken();
+                int length = s2.length();
+                while(s2.length() >= 4) {
+                    sb.append("AAAA");
+                    s2 = s2.substring(4, s2.length());
+                }
+                while(s2.length() >= 2) {
+                    sb.append("BB");
+                    s2 = s2.substring(2, s2.length());
+                }
+                if(s2.length() == 1) {
+                    System.out.println(-1);
+                    return;
+                }
+                index += length;
+            }
+        }
+        System.out.println(sb);
     }
 }
