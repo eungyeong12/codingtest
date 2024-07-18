1ac9f506a72a3f6bc80179869c54ccf182d791e1
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2024-07-18 17:12:51 +0900

    [Bronze IV] Title: !밀비 급일, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(br.readLine());
+            if(sb.toString().equals("END")) break;
+            System.out.println(sb.reverse());
+        }
+    }
+}
