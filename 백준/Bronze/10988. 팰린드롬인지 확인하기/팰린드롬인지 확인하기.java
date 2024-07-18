62dd078d49a1f2a95dbd1b53fb322751efa0b4d4
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2024-07-18 17:21:11 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 208 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        boolean isPalindrome = true;
+        for(int i=0; i<s.length()/2; i++) {
+            if(s.charAt(i) != s.charAt(s.length()-1-i))
+                isPalindrome = false;
+        }
+
+        if(isPalindrome)
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+}
