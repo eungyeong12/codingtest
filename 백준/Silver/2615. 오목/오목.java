4469f9c9f7f90dfeb1dc72579db6680270609673
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2024-08-05 02:12:42 +0900

    [Silver I] Title: 오목, Time: 124 ms, Memory: 16136 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -5,35 +5,44 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N = 19;
-    static int[][] board;
-    static int[] dx = {0, -1, 1, 1}; // 우, 우상, 우하, 하
-    static int[] dy = {1, 1, 1, 0};
+    static final int MAX = 19;
+    static int[][] board = new int[MAX+2][MAX+2];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        board = new int[N][N];
         String s;
         StringTokenizer st;
 
-        for(int i=0; i<N; i++) {
+        for(int i=1; i<=MAX; i++) {
             s = br.readLine();
             st = new StringTokenizer(s, " ");
-            for(int j=0; j<N; j++) {
+            for(int j=1; j<=MAX; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
+        for(int i=1; i<=MAX; i++) {
+            for(int j=1; j<=MAX; j++) {
                 if(board[i][j] != 0) {
-                    int current = board[i][j];
-                    for(int d=0; d<4; d++) {
-                        if(check(i, j, current, d)) {
-                            System.out.println(current);
-                            System.out.println((i+1) + " " + (j+1));
-                            return;
-                        }
+                    if(board[i][j-1] != board[i][j] && search1(board[i][j], i, j) == 1) {
+                        System.out.println(board[i][j]);
+                        System.out.println(i + " " + j);
+                        return;
+                    }
+                    if(board[i-1][j-1] != board[i][j] && search2(board[i][j], i, j) == 1) {
+                        System.out.println(board[i][j]);
+                        System.out.println(i + " " + j);
+                        return;
+                    }
+                    if(board[i-1][j] != board[i][j] && search3(board[i][j], i, j) == 1) {
+                        System.out.println(board[i][j]);
+                        System.out.println(i + " " + j);
+                        return;
+                    }
+                    if(board[i+1][j-1] != board[i][j] && search4(board[i][j], i, j) == 1) {
+                        System.out.println(board[i][j]);
+                        System.out.println(i + " " + j);
+                        return;
                     }
                 }
             }
@@ -42,37 +51,41 @@ public class Main {
         System.out.println(0);
     }
 
-    private static boolean check(int x, int y, int n, int d) {
-        int count = 1;
-        int nx, ny;
-
-        for(int i=1; i<5; i++) {
-            nx = x + i * dx[d];
-            ny = y + i * dy[d];
-
-            if (nx < 0 || nx >= N || ny < 0 || ny >= N || board[nx][ny] != n) {
-                break;
-            }
-            count++;
+    static int search1(int color, int i, int j) {
+        int cnt = 1;
+        while (board[i][j+1] == color) {
+            j++;
+            cnt++;
         }
+        return cnt == 5 && board[i][j+1] != color ? 1 : 0;
+    }
 
-        if(count == 5) {
-            // 육목 확인
-            nx = x - dx[d];
-            ny = y - dy[d];
-            if(nx >= 0 && nx < N && ny >= 0 && ny < N && board[nx][ny] == n) {
-                return false;
-            }
-
-            nx = x + 5 * dx[d];
-            ny = y + 5 * dy[d];
-            if(nx >= 0 && nx < N && ny >= 0 && ny < N && board[nx][ny] == n) {
-                return false;
-            }
-
-            return true;
+    static int search2(int color, int i, int j) {
+        int cnt = 1;
+        while (board[i+1][j+1] == color) {
+            i++;
+            j++;
+            cnt++;
         }
+        return cnt == 5 && board[i+1][j+1] != color ? 1 : 0;
+    }
 
-        return false;
+    static int search3(int color, int i, int j) {
+        int cnt = 1;
+        while (board[i+1][j] == color) {
+            i++;
+            cnt++;
+        }
+        return cnt == 5 && board[i+1][j] != color ? 1 : 0;
+    }
+
+    static int search4(int color, int i, int j) {
+        int cnt = 1;
+        while (board[i-1][j+1] == color) {
+            i--;
+            j++;
+            cnt++;
+        }
+        return cnt == 5 && board[i-1][j+1] != color ? 1 : 0;
     }
 }
