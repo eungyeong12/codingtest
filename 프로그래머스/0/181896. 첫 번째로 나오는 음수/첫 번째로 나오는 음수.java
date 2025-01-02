e02d445a4fe6d0d5ba48365a0e8f01e1333a5e22
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2025-01-02 18:18:36 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.05 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        boolean b = false;
+        for (int i=0; i<num_list.length; i++) {
+            if (num_list[i] < 0) {
+                answer = i;
+                b = true;
+                break;
+            }
+        }
+        if (!b) {
+            answer = -1;
+        }
+        return answer;
+    }
+}
