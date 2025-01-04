14087ee0cec42ae43712bc3ebb1411a84a298d54
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2025-01-04 17:08:09 +0900

    [level 0] Title: 5명씩, Time: 3.18 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -4,10 +4,8 @@ class Solution {
     public String[] solution(String[] names) {
         String[] answer = {};
         List<String> list = new ArrayList<>();
-        for (int i=0; i<names.length; i++) {
-            if (i % 5 == 0) {
-                list.add(names[i]);
-            }
+        for (int i=0; i<names.length; i+=5) {
+            list.add(names[i]);
         }
         return list.toArray(String[]::new);
     }
