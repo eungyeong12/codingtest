ffd67ea6bd5f14158a632dd35077a7b36af61c24
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2025-01-04 16:55:57 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 5.76 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String number) {
         int answer = 0;
         for (int i=0; i<number.length(); i++) {
-            answer += Integer.parseInt(String.valueOf(number.charAt(i)));
+            answer += number.charAt(i) - '0';
         }
         return answer % 9;
     }
