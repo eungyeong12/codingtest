0942f36f1fc605d063d1bd980c2404e54bd0fc28
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2025-01-11 03:44:30 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        for (int i=1; i<=n; i++) {
+            answer *= i;
+            if (answer > n) {
+                return i-1;
+            } else if (answer == n) {
+                return i;
+            }
+        }
+        return answer;
+    }
+}
