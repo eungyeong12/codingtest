1cec8868bd3a6e1a0da9fdf303ee6252826f1282
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2025-01-25 01:57:25 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,61 +1,55 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-class Pair {
-    int x;
-    int y;
+class Main {
+    static int n;
+    static int m;
+    static int[][] board;
+    static int[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-    public Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
+    public static void bfs(int x, int y) {
+        Deque<int[]> queue = new ArrayDeque<>();
+        visited[x][y]++;
+        queue.add(new int[]{x, y});
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        StringTokenizer st = new StringTokenizer(s, " ");
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] board = new int[n][m];
-        int[][] dist = new int[n][m];
-        int[] dx = {1,0,-1,0};
-        int[] dy = {0,1,0,-1};
+        while (!queue.isEmpty()) {
+            int[] p = queue.poll();
+            int px = p[0];
+            int py = p[1];
 
-        for(int i=0; i<n; i++) {
-            s = br.readLine();
-            for(int j=0; j<m; j++) {
-                board[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
-            }
-        }
+            for (int i=0; i<4; i++) {
+                int nx = px + dx[i];
+                int ny = py + dy[i];
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                dist[i][j] = -1; // -1로 초기화하면 visit 배열 사용하지 않아도 됨
-            }
-        }
-
-        ArrayDeque<Pair> queue = new ArrayDeque<>();
-        queue.add(new Pair(0,0));
-        dist[0][0] = 0;
-
-        while(!queue.isEmpty()) {
-            Pair p = queue.poll();
-            for(int k=0; k<4; k++) {
-                int nx = p.x + dx[k];
-                int ny = p.y + dy[k];
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if(board[nx][ny] == 1 && dist[nx][ny] == -1) {
-                    dist[nx][ny] = dist[p.x][p.y]+1;
-                    queue.add(new Pair(nx, ny));
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    if (visited[nx][ny] == 0 && board[nx][ny] == 1) {
+                        visited[nx][ny] = visited[px][py] + 1;
+                        queue.add(new int[]{nx, ny});
+                    }
                 }
             }
         }
+    }
 
-        System.out.println(dist[n-1][m-1]+1); // 문제의 특성상 거리+1이 정답
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+        visited = new int[n][m];
+
+        for (int i=0; i<n; i++) {
+            String s = br.readLine();
+            for (int j=0; j<m; j++) {
+                board[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        bfs(0, 0);
+
+        System.out.println(visited[n-1][m-1]);
     }
 }
