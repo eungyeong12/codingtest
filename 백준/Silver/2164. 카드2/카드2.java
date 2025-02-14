d65769b7ccfacc93cac533a2a8316d480e961bcf
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2025-02-15 00:39:22 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 28980 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,23 +1,20 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Deque<Integer> deque = new ArrayDeque<>();
-
-        for(int i=1; i<=n; i++) {
-            deque.push(i);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> q = new ArrayDeque<>();
+        for (int i=1; i<=n; i++) {
+            q.add(i);
         }
-
-        while(deque.size() != 1) {
-            deque.pollLast();
-            n = deque.pollLast();
-            deque.push(n);
+        while (q.size() > 1) {
+            q.poll();
+            if (q.size() == 1)
+                break;
+            q.add(q.poll());
         }
-
-        System.out.println(deque.poll());
+        System.out.println(q.poll());
     }
 }
