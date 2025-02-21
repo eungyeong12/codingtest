9ece64879c1ed822cefae707c2fc7cf6c896ebcc
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2025-02-22 00:38:39 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 672 ms, Memory: 37528 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,33 +7,30 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<n; i++) {
+        Map<String, Integer> words = new HashMap<>();
+        for (int i=0; i<n; i++) {
             String s = br.readLine();
-            if(s.length() < m) continue;
-            if(map.containsKey(s)) {
-                map.replace(s, map.get(s) + 1);
+            if (s.length() < m) continue;
+            if (words.isEmpty() || !words.containsKey(s)) {
+                words.put(s, 1);
             } else {
-                map.put(s, 1);
+                words.replace(s, words.get(s) + 1);
             }
         }
-
-        List<String> list = new ArrayList<>(map.keySet());
-
-        Collections.sort(list, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if(map.get(o1) != map.get(o2))
-                    return map.get(o2) - map.get(o1);
-                if(o1.length() != o2.length())
-                    return o2.length() - o1.length();
-                return o1.compareTo(o2);
-            }
+        
+        List<String> list = new ArrayList<>(words.keySet());
+        Collections.sort(list, (a, b) -> {
+            if (words.get(a) != words.get(b))
+                return words.get(b) - words.get(a); 
+            if (a.length() != b.length())
+                return b.length() - a.length(); 
+            return a.compareTo(b); 
         });
-
+        
         StringBuilder sb = new StringBuilder();
-        for(String s : list)
-            sb.append(s + "\n");
+        for (String s : list) {
+            sb.append(s).append("\n");
+        }
         System.out.println(sb);
     }
 }
