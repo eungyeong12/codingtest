0782057cf5937fe06a94227bfaf6898794d07b45
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2025-03-21 15:18:00 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 152 ms, Memory: 16712 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.java
@@ -1,78 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
+
+class Room {
+    List<String> players;
+    int minLevel;
+
+    Room(int level, String player) {
+        this.players = new ArrayList<>();
+        players.add(player);
+        this.minLevel = level;
+    }
+    void addPlayer(String player) {
+        players.add(player);
+    }
+}
 
 public class Main {
-    static class Room {
-        int size;
-        boolean isFull = false;
-        ArrayList<String> players;
-
-        Room(int m) {
-            players = new ArrayList<>(m);
-            this.size = m;
-        }
-
-        boolean push(String player) {
-            if(isFull)
-                return false;
-
-            players.add(player);
-
-            if(players.size() == size)
-                isFull = true;
-            return true;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        StringTokenizer st = new StringTokenizer(s, " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int p = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        List<Room> list = new ArrayList<>();
         Map<String, Integer> players = new HashMap<>();
-        ArrayList<Room> list = new ArrayList<>();
-
-        s = br.readLine();
-        st = new StringTokenizer(s, " ");
-        list.add(new Room(m));
-        int l = Integer.parseInt(st.nextToken());
-        String n = st.nextToken();
-        players.put(n, l);
-        list.get(0).push(n);
-
-        for(int i=1; i<p; i++) {
-            s = br.readLine();
-            st = new StringTokenizer(s, " ");
-            l = Integer.parseInt(st.nextToken());
-            n = st.nextToken();
+        for(int i=0; i<p; i++) {
+            st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            String n = st.nextToken();
             players.put(n, l);
-            boolean isEntered = false;
-            for(int j=0; j<list.size(); j++) {
-                int criteria = players.get(list.get(j).players.get(0));
-                if(Math.abs(criteria - l) <= 10)
-                    if(list.get(j).push(n))
-                        isEntered = true;
-                if(isEntered) break;
-            }
-            if(!isEntered) {
-                list.add(new Room(m));
-                list.get(list.size()-1).push(n);
+            if (list.isEmpty()) {
+                list.add(new Room(l, n));
+            } else {
+                boolean b = false;
+                for (Room r : list) {
+                    if (r.players.size() < m && Math.abs(r.minLevel - l) <= 10) {
+                        r.addPlayer(n);
+                        b = true;
+                        break;
+                    }
+                }
+                if (!b) {
+                    list.add(new Room(l, n));
+                }
             }
         }
-
-        for(int i=0; i<list.size(); i++) {
-            if(list.get(i).isFull)
+        for (Room r : list) {
+            Collections.sort(r.players);
+            if (r.players.size() == m) {
                 System.out.println("Started!");
-            else
+            } else {
                 System.out.println("Waiting!");
-
-            Collections.sort(list.get(i).players);
-            for(int j=0; j<list.get(i).players.size(); j++) {
-                String player = list.get(i).players.get(j);
-                System.out.println(players.get(player) + " " + player);
+            }
+            for (int i=0; i<r.players.size(); i++) {
+                System.out.println(players.get(r.players.get(i)) + " " + r.players.get(i));
             }
         }
     }
