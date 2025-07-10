15bc65490b4d111d42afaae66ad5950d62e48b4b
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2025-07-11 00:39:44 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 120 ms, Memory: 20932 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val (a, b) = readLine()!!.split(" ").map { it.toInt() }
+    when {
+        a > b -> println(">")
+        a < b -> println("<")
+        else -> println("==")
+    }
+}
