7258abd4e4c65008ddcf46bc0ac385424817392d
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2025-08-10 00:03:38 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 76 ms, Memory: 12220 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val input = readln()
+    when (input) {
+        "NLCS" -> println("North London Collegiate School")
+        "BHA" -> println("Branksome Hall Asia")
+        "KIS" -> println("Korea International School")
+        "SJA" -> println("St. Johnsbury Academy")
+    }
+}
