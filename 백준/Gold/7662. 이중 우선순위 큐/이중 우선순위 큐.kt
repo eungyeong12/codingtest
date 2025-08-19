713992949adf2c2d789f5b04ab9e21b77c833e87
eungyeong12 <108224432+eungyeong12@users.noreply.github.com>
2025-08-19 22:44:11 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2348 ms, Memory: 427664 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.kt
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.kt
@@ -4,50 +4,20 @@ fun main() = with(System.`in`.bufferedReader()) {
     val t = readLine().toInt()
     repeat(t) {
         val k = readLine().toInt()
-        val minHeap = PriorityQueue<Int>()
-        val maxHeap = PriorityQueue<Int>(Comparator.reverseOrder())
-        val countMap = HashMap<Int, Int>()
-        
-        fun removeInvalidValues(heap: PriorityQueue<Int>) {
-            while (heap.isNotEmpty() && countMap.getOrDefault(heap.peek(), 0) == 0) {
-                heap.poll()
-            }
-        }
-        
+        val map = TreeMap<Int, Int>()
         repeat(k) {
             val (command, numStr) = readLine().split(" ")
             val num = numStr.toInt()
-            
             when (command) {
-                "I" -> {
-                    minHeap.add(num)
-                    maxHeap.add(num)
-                    countMap[num] = countMap.getOrDefault(num, 0) + 1
-                }
-                "D" -> {
-                    if (countMap.isNotEmpty()) {
-                        val targetHeap = if (num == 1) maxHeap else minHeap
-                        removeInvalidValues(targetHeap)
-
-                        if (targetHeap.isNotEmpty()) {
-                            val value = targetHeap.poll()
-                            countMap[value] = countMap[value]!! - 1
-                            if (countMap[value] == 0) {
-                                countMap.remove(value)
-                            }
-                        }
-                    }
+                "I" -> map[num] = (map[num] ?: 0) + 1
+                "D" -> if (map.isNotEmpty()) {
+                    val e = if (num == 1) map.lastEntry() else map.firstEntry()
+                    val cnt = e.value
+                    if (cnt == 1) map.remove(e.key) else map[e.key] = cnt - 1
                 }
             }
         }
-        
-        removeInvalidValues(maxHeap)
-        removeInvalidValues(minHeap)
-        
-        if (countMap.isEmpty()) {
-            println("EMPTY")
-        } else {
-            println("${maxHeap.peek()} ${minHeap.peek()}")
-        }
+        if (map.isEmpty()) println("EMPTY")
+        else println("${map.lastKey()} ${map.firstKey()}")
     }
 }
